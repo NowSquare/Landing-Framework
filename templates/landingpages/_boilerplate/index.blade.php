ea1f08ac872fbc59bc383255a0c8ddefb31589a7
landingframework <info@landingframework.com>
2017-05-13 09:04:12 +0200

    Updated root template

--- templates/landingpages/_boilerplate/index.blade.php
+++ templates/landingpages/_boilerplate/index.blade.php
@@ -51,42 +51,42 @@
         <div class="row">
           <div class="col-12 col-md-4 text-center">
             <div class="row">
-              <div class="col-8 push-2 col-sm-6 push-sm-3 col-lg-6 push-lg-3 text-center">
-                <i class="mb-3 icon-xl color-blue -x-icon mi flight_takeoff" data-attachment="bottom left" data-target-attachment="top left"></i>
+              <div class="col-6 push-3 col-sm-6 push-sm-3 col-lg-6 push-lg-3 text-center">
+                <img src="{{ url('templates/assets/images/icons/electrical_sensor.svg') }}" alt="" class="-x-img img-fluid">
               </div>
               <div class="col-12">
                 <h2 class="mt-1 -x-text">{!! trans('landingpages::block.content_title1') !!}</h2>
                 <p class="lead -x-text">{!! trans('landingpages::block.content_lead1') !!}</p>
                 <p class="btn-container">
-                  <a class="btn btn-outline-blue btn-pill -x-link" href="#" role="button">{!! trans('landingpages::block.content_button') !!}</a>
+                  <a class="btn btn-blue btn-pill -x-link" href="#" role="button">{!! trans('landingpages::block.content_button') !!}</a>
                 </p>
               </div>
             </div>
           </div>
           <div class="col-12 col-md-4 text-center">
             <div class="row">
-              <div class="col-8 push-2 col-sm-6 push-sm-3 col-lg-6 push-lg-3 text-center">
-                <i class="mb-3 icon-xl color-blue -x-icon mi important_devices" data-attachment="bottom left" data-target-attachment="top left"></i>
+              <div class="col-6 push-3 col-sm-6 push-sm-3 col-lg-6 push-lg-3 text-center">
+                <img src="{{ url('templates/assets/images/icons/camcorder.svg') }}" alt="" class="-x-img img-fluid">
               </div>
               <div class="col-12">
                 <h2 class="mt-1 -x-text">{!! trans('landingpages::block.content_title2') !!}</h2>
                 <p class="lead -x-text">{!! trans('landingpages::block.content_lead1') !!}</p>
                 <p class="btn-container">
-                  <a class="btn btn-outline-blue btn-pill -x-link" href="#" role="button">{!! trans('landingpages::block.content_button') !!}</a>
+                  <a class="btn btn-blue btn-pill -x-link" href="#" role="button">{!! trans('landingpages::block.content_button') !!}</a>
                 </p>
               </div>
             </div>
           </div>
           <div class="col-12 col-md-4 text-center">
             <div class="row">
-              <div class="col-8 push-2 col-sm-6 push-sm-3 col-lg-6 push-lg-3 text-center">
-                <i class="mb-3 icon-xl color-blue -x-icon mi room" data-attachment="bottom left" data-target-attachment="top left"></i>
+              <div class="col-6 push-3 col-sm-6 push-sm-3 col-lg-6 push-lg-3 text-center">
+                <img src="{{ url('templates/assets/images/icons/mind_map.svg') }}" alt="" class="-x-img img-fluid">
               </div>
               <div class="col-12">
                 <h2 class="mt-1 -x-text">{!! trans('landingpages::block.content_title3') !!}</h2>
                 <p class="lead -x-text">{!! trans('landingpages::block.content_lead1') !!}</p>
                 <p class="btn-container">
-                  <a class="btn btn-outline-blue btn-pill -x-link" href="#" role="button">{!! trans('landingpages::block.content_button') !!}</a>
+                  <a class="btn btn-blue btn-pill -x-link" href="#" role="button">{!! trans('landingpages::block.content_button') !!}</a>
                 </p>
               </div>
             </div>
@@ -98,7 +98,7 @@
 </section>
 
 <section class="-x-block">
-  <div class="content content-padding-l -x-block-bg-img" style="">
+  <div class="content content-padding-xxl -x-block-bg-img" style="">
     <div class="content-overlay -x-block-bg-color" style="background-color:rgba(255,255,255,1)">
       <div class="container">
         <div class="row">
