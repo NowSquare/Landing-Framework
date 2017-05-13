f2c1cfce7c5ce22ee8051cc0a97be41974353f30
landingframework <info@landingframework.com>
2017-05-13 09:03:55 +0200

    Tweaked responsiveness for some content blocks

--- blocks/landingpages/02-content/block-03-03.blade.php
+++ blocks/landingpages/02-content/block-03-03.blade.php
@@ -5,8 +5,8 @@
         <div class="row">
           <div class="col-12 col-md-4 text-center">
             <div class="row">
-              <div class="col-8 push-2 col-sm-6 push-sm-3 col-lg-6 push-lg-3 text-center">
-                <i class="mb-3 icon-xl color-blue -x-icon mi flight_takeoff" data-attachment="bottom left" data-target-attachment="top left"></i>
+              <div class="col-6 push-3 col-sm-6 push-sm-3 col-lg-6 push-lg-3 text-center">
+                <i class="mb-3 icon-xl color-blue -x-icon mi card_giftcard" data-attachment="bottom left" data-target-attachment="top left"></i>
               </div>
               <div class="col-12">
                 <h2 class="mt-1 -x-text">{!! trans('landingpages::block.content_title1') !!}</h2>
@@ -19,7 +19,7 @@
           </div>
           <div class="col-12 col-md-4 text-center">
             <div class="row">
-              <div class="col-8 push-2 col-sm-6 push-sm-3 col-lg-6 push-lg-3 text-center">
+              <div class="col-6 push-3 col-sm-6 push-sm-3 col-lg-6 push-lg-3 text-center">
                 <i class="mb-3 icon-xl color-blue -x-icon mi important_devices" data-attachment="bottom left" data-target-attachment="top left"></i>
               </div>
               <div class="col-12">
@@ -33,7 +33,7 @@
           </div>
           <div class="col-12 col-md-4 text-center">
             <div class="row">
-              <div class="col-8 push-2 col-sm-6 push-sm-3 col-lg-6 push-lg-3 text-center">
+              <div class="col-6 push-3 col-sm-6 push-sm-3 col-lg-6 push-lg-3 text-center">
                 <i class="mb-3 icon-xl color-blue -x-icon mi room" data-attachment="bottom left" data-target-attachment="top left"></i>
               </div>
               <div class="col-12">
