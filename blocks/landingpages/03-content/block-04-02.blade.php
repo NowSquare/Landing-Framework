36de0472205a58ffedf10cfd1001e6c6c3f4a373
landingframework <info@landingframework.com>
2017-06-07 10:08:46 +0200

    Added content blocks

---
+++ blocks/landingpages/03-content/block-04-02.blade.php
@@ -0,0 +1,26 @@
+<section class="-x-block">
+  <div class="content text-light content-padding-l -x-block-bg-img" style="background-image:url()">
+    <div class="content-overlay -x-block-bg-color" style="background-color:rgba(38,50,56,1)">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="content-padding">
+              <div class="-x-text">
+                <h1>{!! trans('landingpages::block.get_full_access_now') !!}</h1>
+                  <p class="lead">
+                  Pan tu language extrahite immediatemente, e sed effortio anteriormente. O con libere addresses movimento, uno tu scientia summarios. Un que tempore scientia incorporate, con articulos promotores demonstrate da. Tu celos language uso, del ma servi specimen preparation. O pro debe patre scientific, toto inviar intention sed o, es ille apprende finalmente que. Per de hodie parolas, uso da secundo responder.
+                  </p>
+                  <p class="lead">
+                  E web debe vices, hodie instruite web un. O facto capital sia, auxiliar americas abstracte qui de. Gode vices angloromanic del o, pardona anglese in que. Se non patre interlingua secundarimente, major addresses questiones lo pan.
+                  </p>
+                  <p class="lead">
+                  Via texto parlar medical un. Asia personas periodicos es del, per da libera lateres litteratura. Nomina specimen uso un, instruction anteriormente lo non, un pro commun responder. Anglese brevissime que al. Su major nomina intermediari web.
+                  </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
