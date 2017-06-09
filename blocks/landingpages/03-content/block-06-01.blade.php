e9450384c79041110257a95b902e47c577da44d2
landingframework <info@landingframework.com>
2017-06-09 13:22:56 +0200

    Added two content (logo) blocks

---
+++ blocks/landingpages/03-content/block-06-01.blade.php
@@ -0,0 +1,13 @@
+<section class="-x-block">
+  <div class="content text-light content-padding-l -x-block-bg-img" style="background-image:url()">
+    <div class="content-overlay -x-block-bg-color" style="background-color:rgba(33,33,33,1)">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 text-center">
+            <img src="{{ url('/templates/assets/images/logos/logo-white.svg') }}" class="-x-img" data-offset="5px 5px" data-attachment="top right" data-target-attachment="top left">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
