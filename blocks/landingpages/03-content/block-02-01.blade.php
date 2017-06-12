63a843728a1a89fce8cb770d7601d00c549e9a38
landingframework <info@landingframework.com>
2017-06-12 12:34:32 +0200

    Fixed block class

--- blocks/landingpages/03-content/block-02-01.blade.php
+++ blocks/landingpages/03-content/block-02-01.blade.php
@@ -1,5 +1,5 @@
 <section class="-x-block">
-  <div class="content content-padding-none -x-block-bg-img" style="">
+  <div class="content content-padding-l -x-block-bg-img" style="">
     <div class="content-overlay -x-block-bg-color" style="background-color:rgba(255,255,255,1)">
       <div class="container">
         <div class="row">
