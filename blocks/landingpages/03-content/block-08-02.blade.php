3c4a67869f490b28ec8143d834f58a39df84d43b
landingframework <info@landingframework.com>
2017-06-12 10:31:56 +0200

    Removed padding

--- blocks/landingpages/03-content/block-08-02.blade.php
+++ blocks/landingpages/03-content/block-08-02.blade.php
@@ -2,11 +2,11 @@
   <div class="content text-light content-padding-none">
     <div class="content-overlay -x-block-bg-color" style="background-color:rgba(37,37,37,1)">
         <div class="row">
-          <div class="col-12 col-sm-12 push-md-8 col-md-4 push-lg-6 col-lg-6">
+          <div class="col-12 col-sm-12 push-md-8 col-md-4 push-lg-6 col-lg-6 p-0">
             <div class="bg-img -x-block-bg-img min-height-500" style="background-image: url('{{ url('templates/assets/images/photos/sky-woman-clouds-vr.jpg') }}');">
             </div>
           </div>
-          <div class="col-12 col-sm-12 pull-md-4 col-md-8 pull-lg-6 col-lg-6">
+          <div class="col-12 col-sm-12 pull-md-4 col-md-8 pull-lg-6 col-lg-6 p-0">
             <div class="content-padding p-3 m-3 mt-sm-4 mb-sm-4 pt-sm-5 pb-sm-5 m-lg-5 p-lg-5">
               <div class="-x-text">
                 <h2>{!! trans('landingpages::block.content_title7') !!}</h2>
