feb966b6bf70caaad76ad16d54bef52d0953fc8b
landingframework <info@landingframework.com>
2017-06-13 09:32:00 +0200

    Added 2 testimonial blocks

---
+++ blocks/landingpages/08-testimonials/block-03-02.blade.php
@@ -0,0 +1,43 @@
+<section class="-x-block">
+  <div class="content text-light content-padding-l -x-block-bg-img" style="">
+    <div class="content-overlay -x-block-bg-color" style="background-color:rgba(38,50,56,1)">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 text-center">
+            <div class="-x-text">
+              <h2 class="display-4">{!! trans('landingpages::block.what_our_customers_are_saying') !!}</h2>
+            </div>
+          </div>
+          <div class="col-12 col-md-6 text-center mt-5 mt-md-4">
+            <i class="mi format_quote icon-5 -x-icon color-blue-grey" data-offset="5px 5px" data-attachment="top right" data-target-attachment="top left"></i>
+            <blockquote class="-x-text">
+              <p class="lead">{!! trans('landingpages::block.testimonial01') !!}</p>
+              <small>&mdash; <strong>{!! trans('landingpages::block.testimonial_name01') !!}</strong>, {!! trans('landingpages::block.testimonial_company01') !!}</small>
+            </blockquote>
+          </div>
+          <div class="col-12 col-md-6 text-center mt-5 mt-md-4">
+            <i class="mi format_quote icon-5 -x-icon color-blue-grey" data-offset="5px 5px" data-attachment="top right" data-target-attachment="top left"></i>
+            <blockquote class="-x-text">
+              <p class="lead">{!! trans('landingpages::block.testimonial02') !!}</p>
+              <small>&mdash; <strong>{!! trans('landingpages::block.testimonial_name02') !!}</strong>, {!! trans('landingpages::block.testimonial_company02') !!}</small>
+            </blockquote>
+          </div>
+          <div class="col-12 col-md-6 text-center mt-5 mt-md-4">
+            <i class="mi format_quote icon-5 -x-icon color-blue-grey" data-offset="5px 5px" data-attachment="top right" data-target-attachment="top left"></i>
+            <blockquote class="-x-text">
+              <p class="lead">{!! trans('landingpages::block.testimonial03') !!}</p>
+              <small>&mdash; <strong>{!! trans('landingpages::block.testimonial_name03') !!}</strong>, {!! trans('landingpages::block.testimonial_company03') !!}</small>
+            </blockquote>
+          </div>
+          <div class="col-12 col-md-6 text-center mt-5 mt-md-4">
+            <i class="mi format_quote icon-5 -x-icon color-blue-grey" data-offset="5px 5px" data-attachment="top right" data-target-attachment="top left"></i>
+            <blockquote class="-x-text">
+              <p class="lead">{!! trans('landingpages::block.testimonial04') !!}</p>
+              <small>&mdash; <strong>{!! trans('landingpages::block.testimonial_name04') !!}</strong>, {!! trans('landingpages::block.testimonial_company04') !!}</small>
+            </blockquote>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
