c9544bad0a62d14ea463f595878b52894732c260
landingframework <info@landingframework.com>
2017-06-15 20:42:48 +0200

    Added empty template

--- templates/landingpages/_boilerplate/config.php
+++ templates/landingpages/_boilerplate/config.php
@@ -3,7 +3,7 @@
 return [
   'name' => 'Boilerplate',
   'active' => false,
-  'categories' => ['sales_pages', 'company_pages', 'food_drinks', 'video_promotion', 'personal', 'coming_soon', 'thank_you'],
+  'categories' => ['sales_pages', 'company_pages', 'food_drinks', 'video_promotion', 'personal', 'coming_soon', 'thank_you', 'other'],
   'created_at' => '2017-05-13',
   'updated_at' => '2017-05-13',
   'author_name' => 'Landing Framework',
---
+++ templates/landingpages/empty/index.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Empty</title>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link rel="stylesheet" type="text/css" href="{{ url('assets/bs4/css/style.min.css') }}" />
+  <script src="{{ url('assets/bs4/js/scripts.min.js') }}"></script>
+  <link href="//fonts.googleapis.com/css?family=Dosis:200,400|Open+Sans:300,400,700" rel="stylesheet">
+  <style type="text/css">
+    body {
+      font-family: 'Open Sans', sans-serif;
+      font-weight: 300;
+    }
+
+    h1, h2, h3, h4, h5, h6 {
+      font-family: 'Dosis', sans-serif;
+      font-weight: 300 !important;
+    }
+  </style>
+</head>
+<body>
+
+</body>
+</html>
