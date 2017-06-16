3c2f8f3efaf6f4f12d121d6c2756c2baaca655f7
landingframework <info@landingframework.com>
2017-06-16 10:17:43 +0200

    Added simple video template

---
+++ templates/landingpages/simple-video/config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+  'name' => 'Simple Video',
+  'active' => true,
+  'categories' => ['video_promotion'],
+  'created_at' => '2017-06-16',
+  'updated_at' => '2017-06-16',
+  'author_name' => 'Landing Framework',
+  'author_email' => 'info@landingframework.com'
+];
