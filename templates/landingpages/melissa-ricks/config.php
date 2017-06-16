a4e3d96eee4c6310797660940de8136fd00cefb5
landingframework <info@landingframework.com>
2017-06-16 22:10:01 +0200

    Added personal template

---
+++ templates/landingpages/melissa-ricks/config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+  'name' => 'Melissa Ricks',
+  'active' => true,
+  'categories' => ['personal'],
+  'created_at' => '2017-06-16',
+  'updated_at' => '2017-06-16',
+  'author_name' => 'Landing Framework',
+  'author_email' => 'info@landingframework.com'
+];
