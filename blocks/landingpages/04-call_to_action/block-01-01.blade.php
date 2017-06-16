1337e11dbbc753ba8faa5b7853e73f77405d773b
landingframework <info@landingframework.com>
2017-06-16 09:19:34 +0200

    Added call to action blocks

---
+++ blocks/landingpages/04-call_to_action/block-01-01.blade.php
@@ -0,0 +1,21 @@
+<section class="-x-block">
+  <div class="content content-padding -x-block-bg-img" style="">
+    <div class="content-overlay -x-block-bg-color" style="background-color:rgba(0,0,0,0.15)">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+            <div class="-x-text text-center text-sm-left mb-3 mb-sm-0">
+              <h1>{!! trans('landingpages::block.how_can_we_help') !!}</h1>
+              <p class="lead no-margin-b">{!! trans('landingpages::block.how_can_we_help_lead') !!}</p>
+            </div>
+          </div>
+          <div class="col-md-4 text-center text-md-right">
+            <div class="btn-container mt-sm-1">
+              <a class="btn btn-xlg btn-deep-orange -x-link" href="#" data-offset="0px 25px" data-target-attachment="top left" role="button">{!! trans('landingpages::block.contact_us') !!}</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
