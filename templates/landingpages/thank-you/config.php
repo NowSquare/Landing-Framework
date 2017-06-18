33f342b01dc2fc3c308932a6978fb4b88e1348a5
landingframework <info@landingframework.com>
2017-06-18 11:55:46 +0200

    Added thank you template

---
+++ templates/landingpages/thank-you/config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+  'name' => 'Thank You',
+  'active' => true,
+  'categories' => ['thank_you'],
+  'created_at' => '2017-06-18',
+  'updated_at' => '2017-06-18',
+  'author_name' => 'Landing Framework',
+  'author_email' => 'info@landingframework.com'
+];
