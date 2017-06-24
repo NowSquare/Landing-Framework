3fac382915abaf0eed527083336dc073c1db5bdb
landingframework <info@landingframework.com>
2017-06-24 09:28:21 +0200

    Added Happy Homes landing page template

---
+++ templates/landingpages/happy-homes/config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+  'name' => 'Happy Homes',
+  'active' => true,
+  'categories' => ['sales_pages', 'company_pages'],
+  'created_at' => '2017-06-24',
+  'updated_at' => '2017-06-24',
+  'author_name' => 'Landing Framework',
+  'author_email' => 'info@landingframework.com'
+];
