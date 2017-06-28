7acc2fe7e797694d99c273cf277108521b23da69
landingframework <info@landingframework.com>
2017-06-28 10:22:30 +0200

    Video templates config update

--- templates/landingpages/learn-video-marketing/config.php
+++ templates/landingpages/learn-video-marketing/config.php
@@ -3,7 +3,7 @@
 return [
   'name' => 'Learn Video Marketing',
   'active' => true,
-  'categories' => ['video_promotion'],
+  'categories' => ['sales_pages', 'video_promotion'],
   'created_at' => '2017-06-24',
   'updated_at' => '2017-06-24',
   'author_name' => 'Landing Framework',
