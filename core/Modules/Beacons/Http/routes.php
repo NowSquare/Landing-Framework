04ee4c2049700c70074b67640b0c0379fce79312
landingframework <info@landingframework.com>
2017-07-12 14:19:27 +0200

    Removed unused Modules

--- core/Modules/Beacons/Http/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Route::group(['middleware' => 'web', 'prefix' => 'beacons', 'namespace' => 'Modules\Beacons\Http\Controllers'], function()
-{
-    Route::get('/', 'BeaconsController@index');
-});
--- core/Modules/Properties/Resources/views/overview.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('properties::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('properties.name') !!}
-    </p>
-@stop
