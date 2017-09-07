5f30bf1fc297e8be2d6df1730f6f0701f658304b
landingframework <info@landingframework.com>
2017-09-07 10:33:23 +0200

    Fixed issue in case HTTP_ACCEPT_LANGUAGE is not available

--- core/app/Platform/Controllers/Core/Localization.php
+++ core/app/Platform/Controllers/Core/Localization.php
@@ -140,9 +140,10 @@ class Localization extends \App\Http\Controllers\Controller {
 
   public static function getBrowserLocale()
   {
-     // Parse the Accept-Language according to:
-     // http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.4
-     preg_match_all(
+    if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+      // Parse the Accept-Language according to:
+      // http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.4
+      preg_match_all(
         '/([a-z]{1,8})' .       // M1 - First part of language e.g en
         '(-[a-z]{1,8})*\s*' .   // M2 -other parts of language e.g -us
         // Optional quality factor M3 ;q=, M4 - Quality Factor
@@ -150,14 +151,14 @@ class Localization extends \App\Http\Controllers\Controller {
         $_SERVER['HTTP_ACCEPT_LANGUAGE'],
         $langParse);
 
-     $langs = $langParse[1]; // M1 - First part of language
-     $quals = $langParse[4]; // M4 - Quality Factor
+      $langs = $langParse[1]; // M1 - First part of language
+      $quals = $langParse[4]; // M4 - Quality Factor
 
-     $numLanguages = count($langs);
-     $langArr = array();
+      $numLanguages = count($langs);
+      $langArr = array();
 
-     for ($num = 0; $num < $numLanguages; $num++)
-     {
+      for ($num = 0; $num < $numLanguages; $num++)
+      {
         $newLang = strtoupper($langs[$num]);
         $newQual = isset($quals[$num]) ?
            (empty($quals[$num]) ? 1.0 : floatval($quals[$num])) : 0.0;
@@ -166,15 +167,17 @@ class Localization extends \App\Http\Controllers\Controller {
         // primary language.
         $langArr[$newLang] = (isset($langArr[$newLang])) ?
            max($langArr[$newLang], $newQual) : $newQual;
-     }
+      }
 
-     // sort list based on value
-     // langArr will now be an array like: array('EN' => 1, 'ES' => 0.5)
-     arsort($langArr, SORT_NUMERIC);
+      // sort list based on value
+      // langArr will now be an array like: array('EN' => 1, 'ES' => 0.5)
+      arsort($langArr, SORT_NUMERIC);
 
-     // The languages the client accepts in order of preference.
-     $acceptedLanguages = array_keys($langArr);
-
-     return $acceptedLanguages;
+      // The languages the client accepts in order of preference.
+      $acceptedLanguages = array_keys($langArr);
+    } else {
+      $acceptedLanguages = ['EN'];
+    }
+    return $acceptedLanguages;
   }
 }
