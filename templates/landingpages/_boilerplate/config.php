90938c597cbb29f249e66cb62e3b40f16a3600c3
landingframework <info@landingframework.com>
2017-09-15 09:14:19 +0200

    Updated landing page boilerplate

--- templates/landingpages/_boilerplate/config.php
+++ templates/landingpages/_boilerplate/config.php
@@ -4,8 +4,8 @@ return [
   'name' => 'Boilerplate',
   'active' => false,
   'categories' => ['sales_pages', 'company_pages', 'food_drinks', 'video_promotion', 'personal', 'coming_soon', 'thank_you', 'other'],
-  'created_at' => '2017-05-13',
-  'updated_at' => '2017-05-13',
+  'created_at' => '2017-09-15',
+  'updated_at' => '2017-09-15',
   'author_name' => 'Landing Framework',
   'author_email' => 'info@landingframework.com'
 ];
