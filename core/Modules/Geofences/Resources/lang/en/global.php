77509d377949768aac682293f8a08adb8d640d14
landingframework <info@landingframework.com>
2017-10-10 08:43:02 +0200

    Updated geofence limitation message to max 100

--- core/Modules/Geofences/Resources/lang/en/global.php
+++ core/Modules/Geofences/Resources/lang/en/global.php
@@ -32,5 +32,5 @@ return array(
   "latitude" => "Latitude",
   "longitude" => "Longitude",
   "zoom" => "Zoom",
-  "geofence_limit_reached" => "You can't have more than 20 geofences in one iOS app due to iOS limitations. For Android there is a maximum of 100 geofences in one app.",
+  "geofence_limit_reached" => "There is a limit of 100 geofences per app",
 );
