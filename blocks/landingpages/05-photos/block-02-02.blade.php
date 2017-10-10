efb9b4cae3a8534e836ab023981e905b7bae8573
landingframework <info@landingframework.com>
2017-10-10 08:43:35 +0200

    Improved responsiveness of photo block

--- blocks/landingpages/05-photos/block-02-02.blade.php
+++ blocks/landingpages/05-photos/block-02-02.blade.php
@@ -3,10 +3,10 @@
     <div class="content-overlay -x-block-bg-color" style="background-color:rgba(255,255,255,1)">
       <div class="container">
         <div class="row">
-          <div class="col-md-5 push-md-7 order-md-1">
+          <div class="col-md-5 push-md-7">
             <a href="/templates/assets/images/photos/man-holds-digital-tablet-in-his-hands.jpg" data-toggle="lightbox"><img src="/templates/assets/images/photos/man-holds-digital-tablet-in-his-hands.jpg" alt="" class="-x-img img-fluid mdl-shadow--8dp"></a>
           </div>
-          <div class="col-md-7 pull-md-5">
+          <div class="col-md-7 pull-md-5 order-md-1">
             <div class="content-padding">
             <div class="-x-text">
               <h2>{!! trans('landingpages::block.photo_01_head') !!}</h2>
