4550a65f805cf6f934b41c983f00498a094cae50
landingframework <info@landingframework.com>
2018-03-27 13:07:41 +0200

    Updated empty template to use Bootstrap 4.0.0

--- templates/landingpages/empty/index.blade.php
+++ templates/landingpages/empty/index.blade.php
@@ -1,16 +1,22 @@
 <!DOCTYPE html>
-<html>
-<head>
-  <title>Empty</title>
+<html style="">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>Empty template</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!--======================================================
+			Styles
+		======================================================-->
 
-  <link rel="stylesheet" type="text/css" href="{{ url('assets/bs4/css/style.min.css') }}" />
-  <script src="{{ url('assets/bs4/js/scripts.min.js') }}"></script>
-  <link href="//fonts.googleapis.com/css?family=Dosis:200,400|Open+Sans:300,400,700" rel="stylesheet">
-  <style type="text/css">
+    <link rel="stylesheet" type="text/css" href="/assets/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/assets/framework/1.0.0/css/style.min.css">
+
+    <link href="//fonts.googleapis.com/css?family=Dosis:200,400|Open+Sans:300,400,700" rel="stylesheet">
+
+    <style type="text/css">
     body {
       font-family: 'Open Sans', sans-serif;
       font-weight: 300;
@@ -20,9 +26,17 @@
       font-family: 'Dosis', sans-serif;
       font-weight: 300 !important;
     }
-  </style>
-</head>
-<body>
+    </style>
 
-</body>
+		<!--======================================================
+			Scripts (keep in head)
+		======================================================-->
+
+    <script src="/assets/framework/1.0.0/js/scripts.min.js"></script>
+    <script src="/assets/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+  </head>
+  <body>
+
+  </body>
 </html>
