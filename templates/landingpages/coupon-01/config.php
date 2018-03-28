e0392dcd9fe18b5aaa9ae67d4dfcf36fc3f7778a
landingframework <info@landingframework.com>
2018-03-28 10:14:02 +0200

    Added new coupon template

---
+++ templates/landingpages/coupon-01/config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+  'name' => 'Coupon 01',
+  'active' => true,
+  'categories' => ['deals'],
+  'created_at' => '2018-03-28',
+  'updated_at' => '2018-03-28',
+  'author_name' => 'Landing Framework',
+  'author_email' => 'info@landingframework.com'
+];
